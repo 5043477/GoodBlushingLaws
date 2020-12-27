7deff00fb05e7ac043510bb649ef0a569abecbe8
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-12-27 21:11:01 +0000

    e

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ class Main {
 
    int[] Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
      // Array example
-       int[] highScores = new int[11];
+       int[] highScores = new int[10];
        // Add an array of 5 doubles called prices.
 
        // Add an array of 5 Strings called names.
